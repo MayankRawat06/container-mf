5675bb0f53943ff8f697a0d9e0fd44ff2a880a99
Mayank Rawat <mrawat@fastenal.com>
2024-03-09 22:44:57 +0530

    add aos

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './About.scss'
 const About = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <Container className="mt-5 mb-5">
       <Row className="text-center">
@@ -12,6 +17,7 @@ const About = () => {
       <Row className="d-flex justify-content-center mt-5 gap-4">
         <Col className="text-center flex-column">
           <img
+            data-aos="flip-down"
             className="mb-4"
             src="https://img3.hkrtcdn.com/20791/normal_2079092_o.png"
             alt="about-img"
@@ -23,6 +29,7 @@ const About = () => {
         </Col>
         <Col className="text-center flex-column">
           <img
+            data-aos="flip-down"
             className="mb-4"
             src="https://img9.hkrtcdn.com/20791/normal_2079088_o.png"
             alt="about-img"
@@ -34,6 +41,7 @@ const About = () => {
         </Col>
         <Col className="text-center flex-column">
           <img
+            data-aos="flip-down"
             className="mb-4"
             src="https://img1.hkrtcdn.com/20791/normal_2079090_o.png"
             alt="about-img"
--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import DisplayMain from "../../img/displayMain.png";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './Display.scss'
 const Display = () => {
+    useEffect(() => {
+      AOS.init();
+    }, []);
   return (
     <Container className="empty-cart mt-5 mb-5 text-center">
       <Row className="text-center mb-5">
         <h2>Our Range</h2>
       </Row>
       <Row>
-        <Link to='/products'>
-          <img src={DisplayMain} alt="" className="display-img" />
+        <Link to="/products">
+          <img
+            data-aos="zoom-in-up"
+            src={DisplayMain}
+            alt=""
+            className="display-img"
+          />
         </Link>
       </Row>
     </Container>
